2555038b1a03a817be992bbe4b8a9c895dcc06ce
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-04 22:19:14 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,4 +5,9 @@ class Project
     @title = title
     @backers = []
   end
+
+  def add_backer(backer)
+    @backers = backer
+    backer.back_project(self) unless backed_projects.include?(self)
+  end
 end
